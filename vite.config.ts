7f1b74cb6a041c5c20da8b4016e3324390705a15
Sukkoth <suukootj@gmail.com>
2024-09-24 15:50:37 +0300

    trying pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA, type VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugIn: VitePWAOptions = {
+  registerType: "prompt",
+  includeAssets: ["vite.svg"],
+  manifest: {
+    name: "Bible reader appp",
+    short_name: "bible-reader",
+    description: "Bible reading plan for christians",
+    icons: [
+      {
+        src: "/vite.svg",
+        sizes: "192x192",
+        type: "image/svg",
+        purpose: "favicon",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "any",
+  },
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
