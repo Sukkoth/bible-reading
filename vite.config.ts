9c1ffb7f27785e19450757495db7791f28f04814
Sukkoth <suukootj@gmail.com>
2024-09-25 14:59:41 +0300

    remove unused variable

--- vite.config.ts
+++ vite.config.ts
@@ -3,48 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA, type VitePWAOptions } from "vite-plugin-pwa";
 
-const manifestForPlugIn: Partial<VitePWAOptions> = {
-  registerType: "prompt",
-  includeAssets: ["vite.svg"],
-  manifest: {
-    name: "Bible reader appp",
-    short_name: "bible-reader",
-    description: "Bible reading plan for christians",
-    icons: [
-      {
-        src: "/pwa-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/pwa-maskable-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/pwa-maskable-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
-    ],
-    theme_color: "#171717",
-    background_color: "#f0e7db",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "any",
-  },
-};
-
 export default defineConfig({
   plugins: [
     react(),
